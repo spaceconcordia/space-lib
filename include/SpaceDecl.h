f03f5d0260e2bd4c9a17ebe3aeecee775ba26a33
Joseph Martineau <joseph-antoine@spaceconcordia.ca>
2014-06-06 22:02:45 -0400

    SpaceDecl.h

--- include/SpaceDecl.h
+++ include/SpaceDecl.h
@@ -15,11 +15,10 @@
 #include <limits.h>
 
 
-#ifndef CS1_UTEST           /* add -DCS1_UTEST to the ENV flag in the makefile to activate the test environment */
 /*
-* Q6 and PC environment
-*
-*/
+ * Q6 and PC environment
+ */
+
 // Base directories
 #define CS1_APPS            "/home/apps"
 #define CS1_LOGS            "/home/logs"
@@ -28,34 +27,33 @@
 #define CS1_PIDS            "/home/pids"
 #define CS1_TMP             "/tmp"
 
-// Files
-#define CS1_WATCH_PUPPY_PID "/home/pids/watch-puppy.pid"
 
 
-#define NDEBUG              /* disable assertion (assert.h) in production version */
-
-#else // CS1_UTEST is defined!
+#ifdef CS1_UTEST 
+/* add -DCS1_UTEST to the ENV flag in the makefile to activate the test environment */
 /*
-* Unit test environment
-*
-* For unit testing, we prefer not to use the real paths, instead create/populate directories on the fly.
-* Define -DCS1_UTEST in makefile and create/remove test directories in setup()/teardown() 
-*/
+ * Unit test environment
+ *
+ * For unit testing, we prefer not to use the real paths for some directories, 
+ * instead create/populate directories on the fly.
+ * Define -DCS1_UTEST in makefile and create/remove test directories in setup()/teardown() 
+ */
 // Base directories
-#define CS1_UTEST_DIR       "cs1_utest"             // Create this directory within the makefile and use
-#define CS1_APPS            CS1_UTEST_DIR"/apps"    // Create all other within setup() of your tests
+#define CS1_UTEST_DIR       "cs1_utest"             // Create this directory within the makefile
+                                                    // Create all others within setup() of your tests
+#undef CS1_LOGS
 #define CS1_LOGS            CS1_UTEST_DIR"/logs"
+
+#undef CS1_TGZ
 #define CS1_TGZ             CS1_UTEST_DIR"/tgz"
-#define CS1_PIPES           CS1_UTEST_DIR"/pipes"
-#define CS1_PIDS            CS1_UTEST_DIR"/pids"
+
+#undef CS1_TMP
 #define CS1_TMP             CS1_UTEST_DIR
-
-// Files
-#define CS1_WATCH_PUPPY_PID CS1_UTEST_DIR"/pids/watch-puppy.pid"
-
 
 #endif
 
+// Files
+#define CS1_WATCH_PUPPY_PID CS1_PIDS"/watch-puppy.pid"
 
 // Binaries
 #define CS1_BABY_CRON_BIN   CS1_APPS"/current/baby-cron/baby-cron"
@@ -67,13 +65,17 @@
 #define CS1_APPS_NEW        CS1_APPS"/new"
 
 
+// Buffer sizes
 #define CS1_MAX_FRAME_SIZE 190
 #define CS1_TGZ_MAX CS1_MAX_FRAME_SIZE
 
-/* From limits.h */
+// From limits.h
 #define CS1_NAME_MAX NAME_MAX           /* 255 chars in a file name */
 #define CS1_PATH_MAX PATH_MAX           /* 4096 chars in a path name including nul */
 
+#ifndef CS1_UTEST 
+#define NDEBUG              /* disable assertion (assert.h) in production version */
+#endif
 
 
 
