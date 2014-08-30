28d2ad5d34d9c4f4dda3f6dd8469e355fb3de225
Joseph Martineau <jamg85@msn.com>
2014-08-29 20:33:17 -0400

    [SystemProcess.cpp] added missing include that was causing buildQ6 to fail

--- utls/src/SystemProcess.cpp
+++ utls/src/SystemProcess.cpp
@@ -15,6 +15,7 @@
  *
  * =====================================================================================
  */
+#include <iostream>
 #include <stdlib.h>
 #include <stdio.h>
 #include <SystemProcess.h>
@@ -24,19 +25,26 @@
 /**
  * Function to execute a command and get output
  */
-std::string SystemProcess::Execute(char * orig_cmd) {
-  char command[MAX_COMMAND_LENGTH] = {0};
-  sprintf(command, orig_cmd, "2>&1"); // redirect stderr to stdout
-  
-  FILE * exec_pipe = popen(command, "r");
-  if (!exec_pipe) return "[ERROR] failed to open pipe";
-  char buffer[MAX_OUTPUT_LENGTH];
-  std::string result = "";
-  while( !feof(exec_pipe) ) {
-    if ( fgets(buffer, MAX_OUTPUT_LENGTH, exec_pipe) != NULL ) {
-      result+=buffer;
+std::string SystemProcess::Execute(char * orig_cmd) 
+{
+    char command[MAX_COMMAND_LENGTH] = {0};
+    sprintf(command, orig_cmd, "2>&1"); // redirect stderr to stdout
+    
+    FILE * exec_pipe = popen(command, "r");
+
+    if (!exec_pipe) {
+        return "[ERROR] failed to open pipe";
     }
-  }
-  pclose(exec_pipe);
-  return result;
+
+    char buffer[MAX_OUTPUT_LENGTH];
+    std::string result = "";
+
+    while( !feof(exec_pipe) ) {
+        if ( fgets(buffer, MAX_OUTPUT_LENGTH, exec_pipe) != NULL ) {
+            result+=buffer;
+        }
+    }
+
+    pclose(exec_pipe);
+    return result;
 }
