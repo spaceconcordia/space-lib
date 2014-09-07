b2a5dba6f047ed281122f259caf2fa07fb497d91
Space Concordia <olivier@spaceconcordia.ca>
2014-09-07 13:40:37 -0400

    Removed global lib and inc dependency for shakespeare, utls

--- utls/csmake.sh
+++ utls/csmake.sh
@@ -1,22 +1,17 @@
 #!/bin/bash
 #g++ -static -c src/shakespeare.cpp -o lib/shakespeare.o
 
-copy-libs () {
-    cp ./lib/*.a ../lib/
-    cp ./include/* ../include/
-}
-
 build-x86 () {
     make 
-    ar rcs lib/libcs1_utls.a bin/Date.o bin/SpaceString.o && copy-libs
+    ar rcs lib/libcs1_utls.a bin/Date.o bin/SpaceString.o
 }
 build-Q6 () {
     make buildQ6
-    ar rcs lib/libcs1_utls.a bin/Date.o bin/SpaceString.o && copy-libs
+    ar rcs lib/libcs1_utls.a bin/Date.o bin/SpaceString.o
 }
 build-BB () {
     make buildBB
-    ar rcs lib/libcs1_utls.a bin/Date.o bin/SpaceString.o && copy-libs
+    ar rcs lib/libcs1_utls.a bin/Date.o bin/SpaceString.o
 }
 
 build-x86
